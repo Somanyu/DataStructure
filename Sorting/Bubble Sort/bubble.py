16bc3c488d14d250e50562c0cfd5b21caf67a6d8
Somanyu <somanyu.03samal@gmail.com>
2021-02-06 12:27:12 +0530

    Created bubble.py

---
+++ Sorting/Bubble Sort/bubble.py
@@ -0,0 +1,21 @@
+#  function to perform the bubble sort
+
+def bubblesort(arr):
+
+    # outer loop 
+    for i in range(len(arr)):
+
+        # inner loop for swapping elements 
+        for j in range(0, len(arr)-1-i):
+
+            # swapping is performed if
+            # a element is greater than next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+a = [12, 90, 66, 34, 78, 51, 6]
+bubblesort(a)
+
+print("Bubble Sorted --> ", end=" ")
+for i in range(len(a)):
+    print(a[i], end=" ")
