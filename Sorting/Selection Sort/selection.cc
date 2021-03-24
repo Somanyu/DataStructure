6894c17ae524204a7185babedc673e4bf0a82575
Somanyu <somanyu.03samal@gmail.com>
2021-03-24 09:34:23 +0530

    Created selection.cc

---
+++ Sorting/Selection Sort/selection.cc
@@ -0,0 +1,33 @@
+/* Selection Sort using C++ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // array to be sorted
+    int arr[] = { 10, 50, 60, 20, 5 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    // outer loop for moving
+    // to unsorted subarray
+    for(int i = 0; i < n; i++) {
+
+        // inner loop for
+        // comparsion between two adjacent values
+        for(int j = i+1; j < n; j++) {
+
+            // if index of i is greater than index of j
+            // swapping takes place
+            if(arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout << "Sorted Array ->";
+    for(int k = 0; k < n; k++) {
+        cout << " " << arr[k];
+    }
+}
