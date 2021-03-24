77ad0ddc9c969a7d79e6b38c9ff2894d61d6d61e
Somanyu <somanyu.03samal@gmail.com>
2021-03-24 09:30:28 +0530

    Updated selection.java

--- Sorting/Selection Sort/selection.java
+++ Sorting/Selection Sort/selection.java
@@ -1,9 +1,20 @@
+/* Selection Sort using Java */
+
 public class selection {
     public static void main(String[] args) {
+        // array to be sorted
         int arr[] = { 10, 50, 60, 20, 5 };
 
+        // outer loop for moving
+        // to unsorted subarray
         for (int i = 0; i < arr.length; i++) {
+
+            // inner loop for
+            // comparsion between two adjacent values
             for (int j = i+1; j < arr.length; j++) {
+
+                // if index of i is greater than index of j
+                // swapping takes place
                 if (arr[i] > arr[j]) {
                     int temp = arr[i];
                     arr[i] = arr[j];
