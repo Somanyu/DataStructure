c014d26f9cce598b1be7d4eb7f315c4600d5b7bc
Somanyu <somanyu.03samal@gmail.com>
2021-03-24 09:26:12 +0530

    Updated selection.py

--- Sorting/Selection Sort/selection.py
+++ Sorting/Selection Sort/selection.py
@@ -1,11 +1,22 @@
-a = [1, 6.1, 3, 4, 5, 6.4, 7, 8, 9, 10]
+""" Selection Sort using python """
 
+# array to be sorted
+a = [10, 50, 60, 20, 5]
+
+# outer loop for moving 
+# to unsorted subarray
 for i in range(len(a)):
+
+    # inner loop for 
+    # comparsion between two adjacent values
     for j in range(i+1, len(a)):
+
+        # if index of i is greater than index of j
+        # swapping takes place
         if a[i] > a[j]:
             a[i], a[j] = a[j], a[i]
 
-print("sorted array")
+print("Sorted Array ->", end=' ')
 for i in range(len(a)):
-    print("%d" % a[i], end=' '),
+    print(a[i], end=' ')
  
