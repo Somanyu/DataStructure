055f4ab684b2b6bc4ee17fa8f15bd9b3ad324914
Somanyu <somanyu.03samal@gmail.com>
2021-03-24 09:32:41 +0530

    Modified selection.c

--- Sorting/Selection Sort/selection.c
+++ Sorting/Selection Sort/selection.c
@@ -1,19 +1,33 @@
+/* Selection Sort using C */
 #include <stdio.h>
 
 int main() {
-    int arr[] = { 10, 90, 45, 88, 13, 65 };
 
-    for (int i = 0; i < sizeof(arr); i++) {
-        for (int j = i+1; j < sizeof(arr); j++) {
-            if (arr[i] > arr[j]) {
+    // array to be sorted
+    int arr[] = { 10, 50, 60, 20, 5 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    // outer loop for moving
+    // to unsorted subarray
+    for(int i = 0; i < n; i++) {
+
+        // inner loop for
+        // comparsion between two adjacent values
+        for(int j = i+1; j < n; j++) {
+
+            // if index of i is greater than index of j
+            // swapping takes place
+            if(arr[i] > arr[j]) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
     }
-    printf("Sorted Array ->");
-    for (int k = 0; k < sizeof(k); k++) {
-        printf(" %d ", arr[k]);
+    printf("Sorted Array-> ");
+    for(int k = 0; k < n; k++) {
+        printf("%d ", arr[k]);
     }
+
+    return 0;
 }
