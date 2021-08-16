614d6593847870fb00c0631c516e812aa73dddfa
Somanyu <somanyu.03samal@gmail.com>
2021-08-16 08:28:47 +0530

    Created tree.c

---
+++ Tree/tree.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *nodeCreate(int value)
+{
+    struct node *nodeNew;
+    nodeNew = (struct node *)malloc(sizeof(struct node));
+
+    nodeNew -> data = value;
+    nodeNew -> left = NULL;
+    nodeNew -> right = NULL;
+
+    return nodeNew;
+}
+
+void main()
+{
+    struct node *root;
+    root = nodeCreate(4);
+    /*
+              4
+            /   \
+          NULL  NULL
+    */
+   root -> left = nodeCreate(3);
+   root -> right = nodeCreate(7);
+   /*
+            4
+         /     \
+        3       7
+      /   \    /  \
+    NULL NULL NULL NULL
+   */
+}
